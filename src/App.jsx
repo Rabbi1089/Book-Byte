647fd13aef22904e24e87c24bf08eb1d17863867
Rabbi1089 <rabbi.ext@gmail.com>
2024-04-12 16:55:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline bg-green-50 text-red-700">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
